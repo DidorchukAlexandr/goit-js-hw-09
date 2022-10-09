16f2e9f9edf33d81fe7a9a8cf669bfe42f4f351c
DidorchukAlexandr <104354949+DidorchukAlexandr@users.noreply.github.com>
2022-10-09 23:09:21 +0300

    homework 9 js

---
+++ src/js/02-timer.js
@@ -0,0 +1,98 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const btnStartTimer = document.querySelector('button[data-start]');
+const dateDays = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
+const textLabel = document.querySelectorAll('.label');
+
+btnStartTimer.setAttribute('disabled', true);
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  altInput: true,
+  altFormat: 'F j, Y',
+  dateFormat: 'Y-m-d',
+  onClose(selectedDates) {
+    btnStartTimer.addEventListener('click', onClickStartTimer);
+    onInput(selectedDates);
+    function onClickStartTimer(ev) {
+      btnStartTimer.setAttribute('disabled', true);
+      const intervalId = setInterval(() => {
+        if (
+          !btnStartTimer.hasAttribute('disabled') ||
+          comparisonDateNum(selectedDates) <= 0
+        ) {
+          clearInterval(intervalId);
+          return;
+        }
+        const arrDate = convertMs(comparisonDateNum(selectedDates));
+        const dateUser = name(arrDate);
+        console.log();
+        setNewDate(dateUser);
+      }, 1000);
+    }
+  },
+};
+
+const fp = flatpickr('#datetime-picker', { ...options });
+
+function onInput(e) {
+  btnStartTimer.setAttribute('disabled', true);
+  if (comparisonDateNum(e) > 0) {
+    btnStartTimer.removeAttribute('disabled');
+    return;
+  } else {
+    Notiflix.Notify.failure('Please choose a date in the future');
+    return;
+  }
+}
+
+function name(params) {
+  params.days = String(params.days).padStart(2, '0');
+  params.hours = String(params.hours).padStart(2, '0');
+  params.minutes = String(params.minutes).padStart(2, '0');
+  params.seconds = String(params.seconds).padStart(2, '0');
+  return params;
+}
+
+function setNewDate(dateUser) {
+  dateDays.textContent = dateUser.days;
+  dataHours.textContent = dateUser.hours;
+  dataMinutes.textContent = dateUser.minutes;
+  dataSeconds.textContent = dateUser.seconds;
+}
+
+function comparisonDateNum(valueInput) {
+  const newDate = new Date();
+  const selDate = valueInput[0];
+
+  return selDate - newDate;
+}
+
